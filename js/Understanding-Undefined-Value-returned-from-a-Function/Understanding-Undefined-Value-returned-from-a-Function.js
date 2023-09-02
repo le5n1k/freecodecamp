770a858605568f5036beaedd3566fcf90ba67e0f
le5n1k <3dimon343@gmail.com>
2023-09-02 17:40:49 +0300

    add this

---
+++ js/Understanding-Undefined-Value-returned-from-a-Function/Understanding-Undefined-Value-returned-from-a-Function.js
@@ -0,0 +1,15 @@
+/*
+Understanding Undefined Value returned from a Function
+*/
+let sum = 0;
+
+function addThree() {
+    sum = sum + 3;
+}
+function addFive() {
+    sum+=5;
+}
+addThree();
+addFive();
+
+console.log(123;
